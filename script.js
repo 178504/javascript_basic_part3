1e584a672a6e6d6694d61333e68937a1905bd0ac
tsujishouta <koshi51455@gmail.com>
2023-06-11 13:59:10 +0900

    second

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 //1. 変数にNumber型を代入してconsole.log()で表示させてください。
 var variableNumber = 178;
-console.log(variableNumber)//178
+console.log(variableNumber);//178
 
 //2 変数にString型を代入してconsole.log()で表示させてください。
 var variableText = "Text";
-console.log(variableText)//Text
+console.log(variableText);//Text
 
 //3. 変数にBoolean型を代入してconsole.log()で表示させてください。
 var variableBoolean = true;
-console.log(variableBoolean)//true
+console.log(variableBoolean);//true
 
 //4. 10と20を足した結果を変数に代入してconsole.log()で表示させてください。
 var addition = 10 + 20;
-console.log(addition)//30
+console.log(addition);//30
 
 //5. 20から10を引いた結果を変数に代入してconsole.log()で表示させてください。
 var subtraction = 20 - 10;
-console.log(subtraction)//10
+console.log(subtraction);//10
 
 //6. 50へ2を掛けた結果を変数に代入してconsole.log()で表示させてください。
 var multiplication = 50 * 2;
-console.log(multiplication)//100
+console.log(multiplication);//100
 
 //7. 20を2で割った結果を変数に代入してconsole.log()で表示させてください。
 var division = 20 / 2;
-console.log(division)//10
+console.log(division);//10
 
 //8. 12を34を結合させた結果を変数に代入してconsole.log()で表示させてください。
 var joinNumber = "12" + "34";
-console.log(joinNumber)//1234
+console.log(joinNumber);//1234
 
